a55a390c84fda5b33001ab4e2329bf247f116f9f
guhhh25 <guhhh25@gmail.com>
2023-03-31 22:39:08 -0300

    fix: new lateral layout

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Img from '../../images/solo-leveling.jpg'
+import Img from "../../images/solo-leveling.jpg";
 import { FunctionComponent } from "react";
 
 interface SideBarProps {
@@ -10,24 +10,30 @@ const SideBar = (props: SideBarProps) => {
   const { item } = props;
 
   return (
-    <div className="bg-[#b10303] h-screen w-64 text-white">
-
-      <div className="bg-black h-48 w-full border-white border-2">
-        <Image src={Img} alt="any" className="w-full h-full" />
+    <div className=" flex flex-col items-center bg-[#1C1C1C] h-screen w-[320px] text-white">
+      <div className=" h-32 w-32 mt-12 mb-12   rounded-full bg-white">
+    
       </div>
       {item.map((item, key) => {
         return (
-          <div
-            key={key}
-            className="flex border-b-2 w-full items-center justify-between px-5 py-4 text-xl hover:bg-[#920a0a] cursor-pointer"
-          >
-            <p>{item.Title}</p>
-            <p>{item.Icon}</p>
+          // eslint-disable-next-line react/jsx-key
+          <div className="w-full">
+            <div
+              key={key}
+              className="flex   w-full items-center justify-between px-5 py-4 text-sm hover:bg-[#920a0a] cursor-pointer"
+            >
+              <p>{item.Title}</p>
+              <p>{item.Icon}</p>
+            
           </div>
-
-          
+          <div className="px-5">
+            <hr/> 
+          </div>
+            </div>
         );
       })}
+
+      <p className="text-xs mt-8">Copyright ©2023 All rights reserved</p>
     </div>
   );
 };
